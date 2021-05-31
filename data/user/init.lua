7c6e63e833087911a5c1435156d2ea39266f3bf3
purhan <purhan01@gmail.com>
2021-05-31 11:07:27 +0530

    custom build

--- data/user/init.lua
+++ data/user/init.lua
@@ -1,13 +1,28 @@
--- put user settings here
--- this module will be loaded after everything else when the application starts
-
+local core = require "core"
 local keymap = require "core.keymap"
 local config = require "core.config"
 local style = require "core.style"
+local command = require "core.command"
 
--- light theme:
--- require "user.colors.summer"
+command.add(nil, {
+    ["shell:compile-run-cpp"] = function()
+      local doc = core.active_view.doc
+      local filename = doc and system.absolute_path(doc.filename)
+      if(doc) then
+        doc:save()
+        os.execute(
+            "g++ -std=c++17 -Wall -Wextra -Wshadow -fsanitize=undefined '" .. filename
+            .. "' -o '" .. filename .. "'.o && '" .. filename .. "'.o < input.txt > output.txt"
+        )
+      end
+    end,
+})
 
 -- key binding:
--- keymap.add { ["ctrl+escape"] = "core:quit" }
-
+keymap.add {
+    ["ctrl+shift+j"] = "root:split-left",
+    ["ctrl+shift+l"] = "root:split-right",
+    ["ctrl+shift+i"] = "root:split-up",
+    ["ctrl+shift+k"] = "root:split-down",
+    ["ctrl+b"] = "shell:compile-run-cpp",
+}
